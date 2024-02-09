6d55edc4d5193ac6aa5ac1af6aca23f793ef174f
surister <surister98@gmail.com>
2024-02-09 16:50:19 +0100

    get_table_names(): Add software test from cratedb-toolkit

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021-2023, Crate.io Inc.
+# Distributed under the terms of the AGPLv3 license, see LICENSE.
+import pytest
+from cratedb_toolkit.testing.testcontainers.cratedb import CrateDBTestAdapter
+
+# Use different schemas for storing the subsystem database tables, and the
+# test/example data, so that they do not accidentally touch the default `doc`
+# schema.
+TESTDRIVE_EXT_SCHEMA = "testdrive-ext"
+TESTDRIVE_DATA_SCHEMA = "testdrive-data"
+
+
+@pytest.fixture(scope="session")
+def cratedb_service():
+    """
+    Provide a CrateDB service instance to the test suite.
+    """
+    db = CrateDBTestAdapter()
+    db.start()
+    yield db
+    db.stop()
