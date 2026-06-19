dcdd4a925de0d38d90027fc155d7a03da75dc7ee
Bilal Tonga <bilaltonga@gmail.com>
2026-06-19 13:16:57 +0200

    Add unit tests for bind and result processors

--- src/sqlalchemy_cratedb/type/binary.py
+++ src/sqlalchemy_cratedb/type/binary.py
@@ -1,9 +1,9 @@
 import base64
 
-import sqlalchemy as sa
+from sqlalchemy import String
 
 
-class LargeBinary(sa.String):
+class LargeBinary(String):
     """A type for large binary byte data.
 
     The :class:`.LargeBinary` type corresponds to a large and/or unlengthed
@@ -18,24 +18,15 @@ class LargeBinary(sa.String):
         if dialect.dbapi is None:
             return None
 
-        # TODO: DBAPIBinary = dialect.dbapi.Binary
-
         def process(value):
             if value is not None:
-                # TODO: return DBAPIBinary(value)
                 return base64.b64encode(value).decode()
             else:
                 return None
 
         return process
 
-    # Python 3 has native bytes() type
-    # both sqlite3 and pg8000 seem to return it,
-    # psycopg2 as of 2.5 returns 'memoryview'
     def result_processor(self, dialect, coltype):
-        if dialect.returns_native_bytes:
-            return None
-
         def process(value):
             if value is not None:
                 return base64.b64decode(value)
